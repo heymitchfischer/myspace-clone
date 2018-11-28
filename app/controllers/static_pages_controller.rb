51e0782995793b9301c1a44e17fa51b0b5e85967
Tyler Rickard <t.rickard7@hotmail.com>
2018-11-27 20:44:59 -0600

    Devise is done, again

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
+
     
   end
+  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'static_pages#index'
   get '/home' => 'static_pages#index'
+  devise_for :users
 end

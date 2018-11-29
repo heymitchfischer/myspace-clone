d59422fb78fbf58c438c057ac03c139f48645925
Jack Lanciloti <jlanciloti713@gmail.com>
2018-11-28 20:11:53 -0600

    Added functionality to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,23 @@ class UsersController < ApplicationController
 before_action :authenticate_user!
 
   def index
-    @users = User.all
+    
+    if params[:name] && params[:name].length > 2
+      @name = params[:name]
+      @users = User.where('name LIKE ?', "%#{@name}%")
+
+      if @users.empty?
+        @error_message = "No results-- displaying all users instead."
+        @users = User.all
+      end
+    elsif params[:name] && params[:name].length < 3
+      @error_message = "Name needs to be more than 2 letters-- displaying all users instead."
+      @users = User.all
+    else
+      @users = User.all
+    end
   end
+        
   
   def show
     if params[:id]
